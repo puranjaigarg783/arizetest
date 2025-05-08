12df9c43c815b23fb94868cbff2848a0bb30756b
puranjaigarg783 <59496822+puranjaigarg783@users.noreply.github.com>
2025-05-07 21:00:53 -0700

    Add calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
-def add(x, y):\n    return x + y\n\ndef subtract(x, y):\n    return x - y\n\ndef multiply(x, y):\n    return x * y\n\ndef divide(x, y):\n    if y == 0:\n        return 
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
